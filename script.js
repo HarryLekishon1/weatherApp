a34fe57163d35c46d21b832b3420506fa14c9138
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-06-23 14:22:10 +0300

    getting elements by id

---
+++ script.js
@@ -0,0 +1,11 @@
+const apiKey = "9d7cde1f6d07ec55650544be1631307e";
+
+const city = document.querySelector(".city");
+const temprature = document.querySelector('.temp');
+const icond = document.querySelector(".icon");
+const humidity = document.querySelector(".humidity")
+const winds = document.querySelector(".wind")
+const desc = document.querySelector(".description")
+const searchcity = document.querySelector(".search-bar")
+const searchButton = document.querySelector(".search button")
+const locationBtn = document.querySelector(".locationBtn")
